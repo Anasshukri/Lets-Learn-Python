c41d5d8f5112f8bf29b5cf614144ff4773332913
xnasx <anasshukri@outlook.com>
2018-02-01 16:24:19 +0800

    update

---
+++ If...Else Loop.py
@@ -0,0 +1,7 @@
+x = int(input("Odd or Even Number?\n"))
+
+if x % 2 is 0:
+    print("It is an even number.")
+
+else:
+    print("It is an odd number.")
---
+++ While... Loop.py
@@ -0,0 +1,6 @@
+x = int(input("Please enter an even number: "))
+
+while x % 2 != 0:
+    x = int(input("Please enter an even number: "))
+
+print(x, " is even.")
